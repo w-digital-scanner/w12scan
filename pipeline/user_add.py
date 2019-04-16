5d280637547bc0bac3c2dd615dc9bc6c12b369c7
boy-hack <34109680@qq.com>
2019-04-17 00:20:33 +0800

    :construction:添加用户脚本

--- pipeline/user_add.py
+++ pipeline/user_add.py
@@ -3,14 +3,40 @@
 # @Time    : 2019/4/15 8:35 PM
 # @Author  : w8ay
 # @File    : user_add.py
+import sys
+
+sys.path.append("../")
+try:
+    from Server import settings
+except ImportError:
+    raise
+
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Server.settings')
+django.setup()
 from application.utils.userinfo import user_add
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 
 if __name__ == '__main__':
-    name = "admin"
-    email = "admin@admin.com"
-    password = "admin"
+    if len(sys.argv) != 4:
+        print("add_user \n usage: python user_admin.py username email password")
+        exit()
+    argv = sys.argv
+    name = argv[1]
+    email = argv[2]
+    password = argv[3]
+    try:
+        validate_email(email)
+
+    except ValidationError:
+        print("email vaildtion error!")
+        exit()
+
     ret = user_add(name, email, password)
     if ret is True:
-        print("add user success")
+        print("add user {} success".format(name))
     else:
-        print("add user faild " + ret)
+        print("add user {} faild ".format(name) + ret)
