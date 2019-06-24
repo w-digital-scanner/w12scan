e863627e9fea314a1ea6c48aebdefdd4f723b9d3
boy-hack <34109680@qq.com>
2019-06-24 23:45:20 +0800

    add build script for English version

--- dockerconf/start.sh
+++ dockerconf/start.sh
@@ -4,6 +4,7 @@ W12SCAN_BASE=/opt/w12scan
 cd $W12SCAN_BASE
 python3 ${W12SCAN_BASE}/manage.py makemigrations
 python3 ${W12SCAN_BASE}/manage.py migrate
+python3 ${W12SCAN_BASE}/manage.py compilemessages
 python3 ${W12SCAN_BASE}/pipeline/elastic.py
 nohup gunicorn Server.wsgi:application -b 127.0.0.1:8080 > ${W12SCAN_BASE}/web.log &
 python3 ${W12SCAN_BASE}/pipeline/user_add.py boyhack w8ay@qq.com boyhack
